4aed08101c7624bcfcc4a3693c7ee8acc3b3c1d4
masud rana <masudranaacb@gmail.com>
2022-03-16 11:57:03 +0600

    card desing simple

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
-
-const roll=222;
-const name='Sakibal Hassan';
-const commonStyle={
-  color:'yello',
-  backgroundColor:'black',
-  padding:'10px'
-}
 function App() {
   return (
     <div className="App">
+      <StudentCard></StudentCard>
+      <StudentCard></StudentCard>
+      <br />
+      <TeacherCard></TeacherCard>
+      <TeacherCard></TeacherCard>
     </div>
   );
 }
-function Student() {
+function StudentCard() {
   return (
-    <div>Name:</div>
+    <div className='student'>
+      <p>Student</p>
+      <h1>Name: Sakibal Hassan</h1>
+      <h3>Roll:222</h3>
+    </div>
+  );
+}
+function TeacherCard() {
+  return (
+    <div className='teacher'>
+      <p>Teacher</p>
+      <h1>Name: Azad Rohoman</h1>
+      <h3>ID:400</h3>
+    </div>
   );
 }
 
